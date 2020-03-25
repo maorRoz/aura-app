03e7bb0e1e4cc2302333a0ae736435b49e489b2f
maorRoz <andory@post.bgu.ac.il>
2020-03-25 22:20:21 +0200

    added saga tests

---
+++ src/store/app/sagas/watchSendUsersFilters.saga.test.ts
@@ -0,0 +1,39 @@
+import { testSaga } from 'redux-saga-test-plan';
+import { sendUsersFiltersSaga } from './watchSendUsersFilters.saga';
+import { sendUsersFilters, saveApps, showLoadAppsFailure } from '../actions';
+import { FakeUsersFiltersDto, FakeApp } from '../../../../test-utils';
+import { SendUsersFilters } from '../types';
+import { GetFilteredApps } from '../api';
+
+describe('watchSendUsersFilters', () => {
+  test('success', () => {
+    const userFilters = FakeUsersFiltersDto();
+
+    const apps = [FakeApp(), FakeApp(), FakeApp()];
+    testSaga(
+      sendUsersFiltersSaga,
+      sendUsersFilters(userFilters) as SendUsersFilters
+    )
+      .next()
+      .call(GetFilteredApps, userFilters)
+      .next({ data: apps })
+      .put(saveApps(apps))
+      .next()
+      .isDone();
+  });
+
+  test('failure', () => {
+    const userFilters = FakeUsersFiltersDto();
+
+    testSaga(
+      sendUsersFiltersSaga,
+      sendUsersFilters(userFilters) as SendUsersFilters
+    )
+      .next()
+      .call(GetFilteredApps, userFilters)
+      .throw(new Error())
+      .put(showLoadAppsFailure())
+      .next()
+      .isDone();
+  });
+});
